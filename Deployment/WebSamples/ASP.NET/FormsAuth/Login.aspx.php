123a4a5e658fc10d80a3788b3984540ff88c1c0f
Jakub Míšek <jakub@devsense.com>
2011-10-29 15:27:10 +0000

    FormsAuth web sample updated
    git-tfs-id: [http://falcon:8080/tfs/Projects]$/Phalanger/Main;C2110

--- Deployment/WebSamples/ASP.NET/FormsAuth/Login.aspx.php
+++ Deployment/WebSamples/ASP.NET/FormsAuth/Login.aspx.php
@@ -1,16 +1,7 @@
 <?
-	import namespace System;
-	import namespace System:::Data;
-	import namespace System:::Configuration;
-	import namespace System:::Collections;
-	import namespace System:::Web;
-	import namespace System:::Web:::Security;
-	import namespace System:::Web:::UI;
-	import namespace System:::Web:::UI:::WebControls;
-	import namespace System:::Web:::UI:::WebControls:::WebParts;
-	import namespace System:::Web:::UI:::HtmlControls;
-
-	partial class Login extends System:::Web:::UI:::Page
+	use System\Web\Security\FormsAuthentication;
+	
+	partial class Login extends \System\Web\UI\Page
 	{
 		protected function ButtonSubmit_Click($sender, $e)
 		{
