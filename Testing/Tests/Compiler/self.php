27527a247de0046b7c172b4ec1ed54e7fb23c750
Jakub Míšek <jakub@devsense.com>
2011-10-31 11:55:20 +0000

    test for self:: added
    git-tfs-id: [http://falcon:8080/tfs/Projects]$/Phalanger/Main;C2118

---
+++ Testing/Tests/Compiler/self.php
@@ -0,0 +1,20 @@
+[expect php]
+[file]
+<?php
+class A {
+    public static function who() {
+        echo __CLASS__;
+    }
+    public static function test() {
+        self::who();
+    }
+}
+
+class B extends A {
+    public static function who() {
+        echo __CLASS__;
+    }
+}
+
+B::test();
+?>
