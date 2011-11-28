eb076f30f50219cc1bdaad71fe152d87419a5b4f
Miloslav Beňo <miloslav@devsense.com>
2011-11-28 18:51:48 +0000

    - Php.Core.Utils renamed to Utilities - comments
    git-tfs-id: [http://falcon:8080/tfs/Projects]$/Phalanger/Main;C2182

--- Source/Core/Utilities/GlobalScope.cs
+++ Source/Core/Utilities/GlobalScope.cs
@@ -8,17 +8,30 @@ using PHP.Core.Reflection;
 using PHP.Core.Binders;
 using PHP.Core.Emit;
 
-namespace PHP.Core.Utils
+namespace PHP.Core.Utilities
 {
+
+    /// <summary>
+    /// Convenience class for accessing global functions and global variables
+    /// </summary>
     public sealed class GlobalScope : DynamicObject
     {
         private ScriptContext context;
 
-        internal GlobalScope(ScriptContext currentContext)
+        /// <summary>
+        /// Initialize GlobalScope object
+        /// </summary>
+        /// <param name="currentContext"></param>
+        public GlobalScope(ScriptContext currentContext)
         {
             context = currentContext;
         }
 
+        #region DynamicObject
+
+        /// <summary>
+        /// Specifies dynamic behavior for invoke operation for global function
+        /// </summary>
         public override bool TryInvokeMember(
             InvokeMemberBinder binder,
             Object[] args,
@@ -29,6 +42,7 @@ namespace PHP.Core.Utils
 
             for(int i = 0; i < args.Length; ++i)
             {
+                Debug.Assert(!(args[i] is PhpReference));
                 wrappedArgs[i] = ClrObject.WrapDynamic(args[i]);
             }
 
@@ -36,6 +50,9 @@ namespace PHP.Core.Utils
             return true;
         }
 
+        /// <summary>
+        /// Specifies dynamic behavior for get operation for global variable
+        /// </summary>
         public override bool TryGetMember(
             GetMemberBinder binder,
             out Object result
@@ -47,14 +64,21 @@ namespace PHP.Core.Utils
             return true;
         }
 
+        /// <summary>
+        /// Specifies dynamic behavior for set operation for global function
+        /// </summary>
         public override bool TrySetMember(
             SetMemberBinder binder,
             Object value
         )
         {
+            Debug.Assert(!(value is PhpReference));
+            
             context.GlobalVariables[binder.Name] = ClrObject.WrapDynamic(value);
             return true;
         }
+
+        #endregion
 
     }
 
