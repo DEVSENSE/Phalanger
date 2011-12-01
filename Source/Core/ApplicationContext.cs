be5a39f5ecb2ee3cf589300b5ee858547e445a6f
Jakub Míšek <jakub@devsense.com>
2011-12-01 15:37:07 +0000

    - BadMethodCallException, BadFunctionCallException - EmptyIterator
    git-tfs-id: [http://falcon:8080/tfs/Projects]$/Phalanger/Main;C2186

--- Source/Core/ApplicationContext.cs
+++ Source/Core/ApplicationContext.cs
@@ -176,20 +176,26 @@ namespace PHP.Core
             addType(typeof(Library.EventClass<>));
             addType(typeof(Library.SPL.ArrayAccess));
             addType(typeof(Library.SPL.Traversable));
-            addType(typeof(Library.SPL.Iterator));
-            addType(typeof(Library.SPL.SeekableIterator));
             addType(typeof(Library.SPL.IteratorAggregate));
-            addType(typeof(Library.SPL.OuterIterator));
-            addType(typeof(Library.SPL.RecursiveIterator));
+
             addType(typeof(Library.SPL.Serializable));
             addType(typeof(Library.SPL.Countable));
-            addType(typeof(Library.SPL.ArrayIterator));
             addType(typeof(Library.SPL.Reflector));
 
+            // Iterators:
+            addType(typeof(Library.SPL.Iterator));
+            addType(typeof(Library.SPL.SeekableIterator));
+            addType(typeof(Library.SPL.OuterIterator));
+            addType(typeof(Library.SPL.RecursiveIterator));
+            addType(typeof(Library.SPL.ArrayIterator));
+            addType(typeof(Library.SPL.EmptyIterator));
+            
             // Exception:
             addType(typeof(Library.SPL.Exception));
             addType(typeof(Library.SPL.LogicException));
-            addType(typeof(Library.SPL.InvalidArgumentException));            
+            addType(typeof(Library.SPL.InvalidArgumentException));
+            addType(typeof(Library.SPL.BadFunctionCallException));
+            addType(typeof(Library.SPL.BadMethodCallException));
 
             // primitive constants
             constants.Add("TRUE", GlobalConstant.True.ConstantDesc, true);
