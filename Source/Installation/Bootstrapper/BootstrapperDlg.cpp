c7f7ad6595579a779070d63cb1dee5ea3c5a225c
Jakub Míšek <jakub@devsense.com>
2012-01-09 19:15:41 +0000

    - vs integration does not display function return type (as it is in C#, and it is more user friendly) - vs integration version updated to .2286 (in installer bootstrapper too)
    git-tfs-id: [http://falcon:8080/tfs/Projects]$/Phalanger/Main;C2286

--- Source/Installation/Bootstrapper/BootstrapperDlg.cpp
+++ Source/Installation/Bootstrapper/BootstrapperDlg.cpp
@@ -119,7 +119,7 @@ HRESULT CBootstrapperDlg::OnLinkCore(IHTMLElement* pElement)
 }
 
 #define PHALANGER_TOOLS_PATH TEXT("\\Microsoft\\VisualStudio\\10.0\\Extensions\\DEVSENSE\\Phalanger Tools Lite")
-#define PHALANGER_TOOLS_VERSION TEXT("3.0.2125") // CURRENT VERSION OF TOOLS // TODO: extract from .VSIX automatically
+#define PHALANGER_TOOLS_VERSION TEXT("3.0.2286") // CURRENT VERSION OF TOOLS // TODO: extract from .VSIX automatically
 
 // recursively delete directory
 bool DeleteDir(TCHAR*szPath)
