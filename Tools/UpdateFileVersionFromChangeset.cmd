f4dfbaa7f39010ce4a6d3c792c702f8204ddb073
Jakub Míšek <jakub@devsense.com>
2012-05-03 14:30:02 +0000

    Automatic pre-build event that updates FileVersionInfo (and other version infos) according to current TFS changeset number. If there is no tf.exe or history, "notfs" is added into the version number.
    git-tfs-id: [http://falcon:8080/tfs/Projects]$/Phalanger/Main;C2855

---
+++ Tools/UpdateFileVersionFromChangeset.cmd
@@ -0,0 +1,17 @@
+call "%VS100COMNTOOLS%\vsvars32.bat"
+
+REM determining changeset number ...
+REM run this batch file from within root folder of your workspace
+
+tf history . /r /noprompt /stopafter:1 /version:W > tf.output 2>nul
+SET CHANGESET=notfs
+FOR /F "tokens=1 skip=2" %%N IN (tf.output) DO @SET CHANGESET=%%N
+del /F /Q tf.output
+
+:start
+if "%1"=="" (goto :main)
+"%0\..\VersionReplacer.exe" "%1" %CHANGESET%
+SHIFT /1
+goto :start
+:main
+REM Done.
---
+++ Tools/VersionReplacer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace VersionReplacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fname = args[0];
+            var changeset = args[1];
+            bool isvsix = fname.EndsWith(".vsixmanifest", StringComparison.InvariantCultureIgnoreCase);
+            bool iscs = fname.EndsWith(".cs", StringComparison.InvariantCultureIgnoreCase);
+
+            Console.WriteLine("Updating version info in '{0}'", fname);
+
+            if (!isvsix && !iscs)
+                return;
+
+            MatchEvaluator versionChanger = (match) =>
+                {
+                    var line = match.Groups[0].Value;
+                    var old = match.Groups[1];
+                    return line.Remove(old.Index - match.Groups[0].Index) + changeset + line.Substring(old.Index - match.Groups[0].Index + old.Length);
+                };
+
+            var result = new StringBuilder();
+            foreach (var line in File.ReadAllLines(fname))
+            {
+                if (isvsix)
+                {
+                    if (line.Contains("<Version>"))
+                    {
+                        var regex = new Regex(@"<Version>[0-9]+\.[0-9]+\.(.+)</Version>");
+                        var newline = (regex.Replace(line, versionChanger));
+                        result.AppendLine(newline);
+                        continue;
+                    }
+                }
+                else if (iscs)
+                {
+                    if (line.Contains("public const string Version"))
+                    {
+                        var regex = new Regex(@"public\s+const\s+string\s+Version\s+\=\s+""[0-9]+\.[0-9]+\.(.+)""");
+                        var newline = (regex.Replace(line, versionChanger));
+                        result.AppendLine(newline);
+                        continue;
+                    }
+
+                    if (line.Contains("[assembly: AssemblyFileVersion"))
+                    {
+                        var regex = new Regex(@"[assembly\:\s+AssemblyFileVersion\(""[0-9]+\.[0-9]+\.[0-9]+\.(.+)""\)");
+                        var newline = (regex.Replace(line, versionChanger));
+                        result.AppendLine(newline);
+                        continue;
+                    }
+                }
+                
+                // copy the line to output:
+                result.AppendLine(line);
+            }
+
+            File.SetAttributes(fname, FileAttributes.Normal);
+            File.WriteAllText(fname, result.ToString());
+        }
+    }
+}
