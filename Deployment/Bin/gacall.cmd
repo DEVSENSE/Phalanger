b2de9dd218d4be5a1e6f2f7b2804102669ae8b77
Jakub Míšek <jakub@devsense.com>
2012-06-14 17:18:40 +0000

    new extensions in deployment folder
    git-tfs-id: [http://falcon:8080/tfs/Projects]$/Phalanger/Main;C3036

--- Deployment/Bin/gacall.cmd
+++ Deployment/Bin/gacall.cmd
@@ -2,7 +2,14 @@ PATH = %PATH%;"C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0
 
 gacutil -f -i PhpNetCore.dll
 gacutil -f -i PhpNetClassLibrary.dll
-gacutil -f -i PhpNetMsSql.dll
-gacutil -f -i PhpNetXmlDom.dll
+
 gacutil -f -i php4ts.dll
 gacutil -f -i php5ts.dll
+
+gacutil -f -i PhpNetXmlDom.dll
+gacutil -f -i PhpNetMsSql.dll
+gacutil -f -i PhpNetPDO.dll
+gacutil -f -i PhpNetPDOSQLite.dll
+gacutil -f -i PhpNetPDOSQLServer.dll
+gacutil -f -i PhpNetSQLite.dll
+gacutil -f -i PhpNetZip.dll
