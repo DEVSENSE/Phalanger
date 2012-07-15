8752c73cd98687e2598a3049ba8c8666e2dc3f93
Jakub Míšek <jakub@devsense.com>
2012-07-15 19:12:59 +0000

    is_numeric() test updated
    git-tfs-id: [http://falcon:8080/tfs/Projects]$/Phalanger/Main;C3141

--- Testing/Tests/Variables/is_number.php
+++ Testing/Tests/Variables/is_number.php
@@ -1,14 +1,14 @@
-[expect exact]
-001001
-440
+[expect php]
 [file]
 <?
-echo is_numeric("+") ? "1":"0";
-echo is_numeric(" ") ? "1":"0";
-echo is_numeric("2.") ? "1":"0";
-echo is_numeric("2.e") ? "1":"0";
-echo is_numeric("2.e+") ? "1":"0";
-echo is_numeric("2.e+1") ? "1":"0";
+function test($x)
+{
+	echo is_numeric($x) ? "1":"0";
+}
+
+foreach (array("+", " ", "2.", "2.e", "2.e+", "2.e+1", ".e2", ".", "1e", "-e2") as $x)
+	test($x);
+
 echo "\n";
 $a = "44e1c";
 echo (double)$a;
