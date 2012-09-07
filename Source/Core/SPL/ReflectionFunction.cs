d6753e8556f7bfa7f8390a4e9bd48352429b34df
Jakub Míšek <jakub@devsense.com>
2012-09-07 15:35:46 +0000

    ReflectionMethod - ReflectionFunction::$name fix - ReflectionClass::getConstructor, getMethods - more stubs
    git-tfs-id: [http://falcon:8080/tfs/Projects]$/Phalanger/Main;C3440

--- Source/Core/SPL/ReflectionFunction.cs
+++ Source/Core/SPL/ReflectionFunction.cs
@@ -372,6 +372,7 @@ namespace PHP.Library.SPL
 
         #region Properties
 
+        [PhpVisible]
         public override string name
         {
             get
@@ -417,7 +418,7 @@ namespace PHP.Library.SPL
         [ImplementsMethod]
         public virtual object __construct(ScriptContext context, object arg)
         {
-            string name = Core.Convert.ObjectToString(arg);
+            string name = PhpVariable.AsString(arg);
             if (!string.IsNullOrEmpty(name))
             {
                 routine = context.ResolveFunction(name, null, false);
@@ -471,12 +472,9 @@ namespace PHP.Library.SPL
             if ((dict = args as IDictionary) != null) values = dict.Values;
             else values = args as ICollection;
 
-            if (values == null)
-                return false;
-
             // invoke the routine
             var stack = context.Stack;
-            stack.AddFrame(values);
+            stack.AddFrame(values ?? ArrayUtils.EmptyObjects);
             return routine.Invoke(null, stack);
         }
 
@@ -500,6 +498,219 @@ namespace PHP.Library.SPL
         {
             stack.RemoveFrame();
             return ((ReflectionFunction)instance).isDisabled(stack.Context);
+        }
+    }
+
+    #endregion
+
+    #region ReflectionMethod
+
+    [ImplementsType]
+    public class ReflectionMethod : ReflectionFunctionAbstract
+    {
+        internal DTypeDesc dtype;
+        internal DRoutineDesc method;
+        
+        #region Constants
+
+        public const int IS_STATIC = 1;
+        public const int IS_PUBLIC = 256;
+        public const int IS_PROTECTED = 512;
+        public const int IS_PRIVATE = 1024;
+        public const int IS_ABSTRACT = 2;
+        public const int IS_FINAL = 4;
+
+        #endregion
+
+        #region Properties
+
+        [PhpVisible]
+        public override string name
+        {
+            get
+            {
+                return (method != null) ? method.MakeFullName() : null;
+            }
+        }
+
+        [PhpVisible]
+        public string @class
+        {
+            get
+            {
+                return (dtype != null) ? dtype.MakeFullName() : null;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// For internal purposes only.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public ReflectionMethod(ScriptContext/*!*/context, bool newInstance)
+            : base(context, newInstance)
+        { }
+
+        /// <summary>
+        /// For internal purposes only.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public ReflectionMethod(ScriptContext/*!*/context, DTypeDesc caller)
+            : base(context, caller)
+        { }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static object __construct(object instance, PhpStack stack)
+        {
+            object argument = stack.PeekValue(1);
+            stack.RemoveFrame();
+            return ((ReflectionFunction)instance).__construct(stack.Context, argument);
+        }
+
+        /// <summary>
+        /// Constructs a ReflectionFunction object.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="arg">The name of the function to reflect or a closure.</param>
+        /// <returns></returns>
+        [ImplementsMethod]
+        public virtual object __construct(ScriptContext context, object @class, object methodname)
+        {
+            string methodnameStr = PhpVariable.AsString(methodname);
+
+            if (@class == null || string.IsNullOrEmpty(methodnameStr))
+                return false;
+
+            this.dtype = null;
+            this.method = null;
+
+            DObject dobj;
+            string str;
+
+            if ((dobj = (@class as DObject)) != null)
+            {
+                this.dtype = dobj.TypeDesc;
+            }
+            else if ((str = PhpVariable.AsString(@class)) != null)
+            {
+                this.dtype = context.ResolveType(str, null, null, null, ResolveTypeFlags.UseAutoload);
+            }
+
+            if (this.dtype == null)
+                return false;
+
+            if (this.dtype.GetMethod(new Name(methodnameStr), dtype, out this.method) == GetMemberResult.NotFound)
+                return false;
+
+            return null;
+        }
+
+        #endregion
+
+        //public static string export ( string $class , string $name [, bool $return = false ] )
+        //public Closure getClosure ( string $object )
+        //public int getModifiers ( void )
+        //public ReflectionMethod getPrototype ( void )
+
+        [ImplementsMethod]
+        public virtual object/*ReflectionClass*/getDeclaringClass(ScriptContext context)
+        {
+            if (dtype == null)
+                return false;
+
+            return new ReflectionClass(context, true)
+            {
+                typedesc = dtype
+            };
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static object getDeclaringClass(object instance, PhpStack stack)
+        {
+            stack.RemoveFrame();
+
+            return ((ReflectionMethod)instance).getDeclaringClass(stack.Context);
+        }
+
+        [ImplementsMethod, NeedsArgless]
+        public virtual object/*mixed*/invoke(ScriptContext context, object instance)
+        {
+            Debug.Fail("ArgLess should be called instead!");
+            throw new InvalidOperationException();
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static object invoke(object instance, PhpStack stack)
+        {
+            var m = (ReflectionMethod)instance;
+            if (m.method == null)
+            {
+                stack.RemoveFrame();
+                return false;
+            }
+
+            object obj = stack.PeekValue(1);
+            PhpArray args = new PhpArray((stack.ArgCount > 1 ? (stack.ArgCount - 1) : (0)));
+            for (int i = 2; i <= stack.ArgCount; i++)
+                args.Add(stack.PeekValue(i));
+            stack.RemoveFrame();
+            
+            //
+            return m.invokeArgs(stack.Context, obj, args);
+        }
+
+        [ImplementsMethod]
+        public virtual object/*mixed*/invokeArgs(ScriptContext context, object instance, object args)
+        {
+            if (method == null)
+                return false;
+
+            var dobj = instance as DObject;
+            if (!method.IsStatic)
+            {
+                if (dobj == null || !this.dtype.IsAssignableFrom(dobj.TypeDesc))    // non static method needs compatible instance
+                {
+                    PhpException.InvalidArgument("instance");
+                    return false;
+                }
+            }
+
+            ICollection values = null;
+            IDictionary dict;
+            if ((dict = args as IDictionary) != null) values = dict.Values;
+            else values = args as ICollection;
+
+            // invoke the routine
+            var stack = context.Stack;
+            stack.AddFrame(values ?? ArrayUtils.EmptyObjects);
+            return method.Invoke(dobj, stack);
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static object invokeArgs(object instance, PhpStack stack)
+        {
+            var args = stack.PeekValue(1);
+            stack.RemoveFrame();
+            
+            return ((ReflectionFunction)instance).invokeArgs(stack.Context, args);
+        }
+
+        //public bool isAbstract ( void )
+        //public bool isConstructor ( void )
+        //public bool isDestructor ( void )
+        //public bool isFinal ( void )
+        //public bool isPrivate ( void )
+        //public bool isProtected ( void )
+        //public bool isPublic ( void )
+        //public bool isStatic ( void )
+        //public void setAccessible ( bool $accessible )
+        
+        public override object/*string*/__toString(ScriptContext context)
+        {
+            throw new NotImplementedException();
         }
     }
 
