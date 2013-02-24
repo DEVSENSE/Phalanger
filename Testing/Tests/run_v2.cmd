f10d148653fadbce4741e8fa092c062a544c3e9e
Ashod Nakashian <ashodnakashian@yahoo.com>
2013-02-23 19:07:38 -0500

    Test runner batch now takes optional command-line arguments.

--- Testing/Tests/run_v2.cmd
+++ Testing/Tests/run_v2.cmd
@@ -1,7 +1,7 @@
 @echo off
-..\..\Tools\PhpNetTester.exe /compiler:..\..\Deployment\Debug\phpc.exe /php:..\..\Tools\PHP\php.exe
+..\..\Tools\PhpNetTester.exe /compiler:..\..\Deployment\Debug\phpc.exe /php:..\..\Tools\PHP\php.exe %*
 del /s *.pdb *.exe EmittedNodes.csv LibraryCalls.csv UnknownCalls.csv __input.txt Debug.log > nul
 
-@echo Deleting empty log files...
-for /f "delims=" %%i in ('dir /s/b/a-d *.log') do if %~zi==0 del %%i
+@rem Deleting empty log files...
+for /f "delims=" %%i in ('dir /s/b/a-d *.log') do if %%~zi==0 del %%i
 pause
