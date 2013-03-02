1666b5bcb976947ec9584b3342c1b6075343cf44
Ashod Nakashian <ashodnakashian@yahoo.com>
2013-03-02 14:01:53 -0500

    Added new project to reproduce and test the autoloader bug that loses ctor params (see msa/main.php).

---
+++ Testing/Tests/PHP.NET/MultiScriptAssembly/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UseMultiScriptAssembly
+{
+    using PHP.Core;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScriptContext context = ScriptContext.InitApplication(ApplicationContext.Default, null, null, null);
+
+            // redirect PHP output to the console:
+            context.Output = Console.Out; // Unicode text output
+            context.OutputStream = Console.OpenStandardOutput(); // byte stream output
+
+            context.Include("main.php", true);
+
+            var klass = (PhpObject)context.NewObject("Klass", new object[] { "yipppy" });
+            var foo = new PhpCallback(klass, "foo");
+            foo.Invoke(null, new object[] { "param" });
+        }
+    }
+}
