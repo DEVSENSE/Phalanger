3ea1f39c090e91ddc027d280a2863f83e2671fe0
Ashod Nakashian <ashodnakashian@yahoo.com>
2013-03-10 16:14:48 -0400

    DateTime modifications now correctly skip invalid time points (as far as .Net knows about them).
    New test added and commented on a case where .Net doesn't know about a change in time.
    Should ultimately consider using Noda Time library for correct and up-to-date date-time handling like PHP.

--- Testing/Tests/@PHP/date/date_modify-1.php
+++ Testing/Tests/@PHP/date/date_modify-1.php
@@ -1,8 +1,19 @@
-[expect php]
+[expectf]
+Thu, 19 Aug 93 23:59:59 -1200
+Sat, 21 Aug 93 00:00:00 +1200
+Sun, 27 Mar 05 01:59:59 +0100
+Sun, 27 Mar 05 03:00:00 +0200
+Sun, 30 Oct 05 01:59:59 +0200
+Sun, 30 Oct 05 03:00:00 +0100
 [file]
 <?php 
   if (!function_exists('date_create')) die("SKIP");
-  
+
+// .Net 4 seems to be unaware that Kwajalein clocks
+// skipped Aug 20 as they went ahead by 24 hours.
+// Also, the timezone on Aug 19 is -12, not +12
+// as it changed from KWAT (UTC-12) to MHT (UTC+12).
+// http://www.timeanddate.com/worldclock/clockchange.html?n=2243&year=1993
 date_default_timezone_set("Pacific/Kwajalein");
 $ts = date_create("Thu Aug 19 1993 23:59:59");
 echo date_format($ts, DateTime::RFC822), "\n";
