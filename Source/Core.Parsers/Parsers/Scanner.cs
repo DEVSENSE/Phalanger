a645e1ad6cd5f31c961d859196e9ff3e8e998fe0
Jakub Míšek <jakub@devsense.com>
2014-07-01 07:57:38 +0000

    T_LINE_COMMENT handled at the end of file properly without newline at the end
    git-tfs-id: [http://falcon:8080/tfs/Projects]$/Phalanger/Main;C5358

--- Source/Core.Parsers/Parsers/Scanner.cs
+++ Source/Core.Parsers/Parsers/Scanner.cs
@@ -455,7 +455,18 @@ namespace PHP.Core.Parsers
 					case Tokens.T_CLOSE_TAG:
                         this.commentsSink.OnCloseTag(this, TokenTextSpan);                        
 						token = Tokens.T_SEMI;
-						goto case Tokens.T_SEMI;
+						goto default;
+
+                    case Tokens.EOF:
+                        if (this.CurrentLexicalState == LexicalStates.ST_ONE_LINE_COMMENT)
+                        {
+                            this.CurrentLexicalState = LexicalStates.ST_IN_SCRIPTING;
+                            token = Tokens.T_LINE_COMMENT;
+                            _yymore();
+                            goto case Tokens.T_LINE_COMMENT;
+                        }
+                        goto default;
+
 
 					case Tokens.T_TRUE:
 					case Tokens.T_FALSE:
