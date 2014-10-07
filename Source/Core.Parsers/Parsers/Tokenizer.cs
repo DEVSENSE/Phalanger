c97065d72bdac1b411f120e751e335e48c145740
Jakub Míšek <jakub@devsense.com>
2014-10-07 15:33:09 +0000

    Tokenizer provides TokenSpan
    git-tfs-id: [http://falcon:8080/tfs/Projects]$/Phalanger/Main;C5574

--- Source/Core.Parsers/Parsers/Tokenizer.cs
+++ Source/Core.Parsers/Parsers/Tokenizer.cs
@@ -58,6 +58,8 @@ namespace PHP.Core.Parsers
 		public TokenCategory TokenCategory { get { return tokenCategory; } }
 		private TokenCategory tokenCategory;
 
+        public Text.Span TokenSpan { get { return Text.Span.FromBounds(token_start_pos.Char, token_end_pos.Char); } }
+
 		public Tokens RealToken { get { return realToken; } }
 		private Tokens realToken;
 
