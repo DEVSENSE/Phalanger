d0f468caeea59a18aa08635f61b5be92a4f6aa0b
Jakub Míšek <jmisek@gmail.com>
2015-02-07 17:35:34 +0000

    - Operators.GetItem(object,object,GetItemKinds) inlined - roadsend test runs in x86 too
    Originally checked in by DEVSENSE\Jakub on 2012-02-17 15:11:06.077 as Changeset 2422
    
    git-tfs-id: [https://devsense.visualstudio.com/DefaultCollection]$/Phalanger/Main;C6478

--- Testing/Benchmarks/roadsend/go.bat
+++ Testing/Benchmarks/roadsend/go.bat
@@ -1,8 +1,11 @@
 ..\..\..\Deployment\Bin\phpc /debug- /config:App.config test_env.php
 ..\..\..\Tools\peverify bin\test_env.exe
-bin\test_env.exe results.csv
+
+REM x64 test run:
+bin\test_env.exe
 
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\corflags.exe" /32BIT+ bin\test_env.exe
-bin\test_env.exe results.csv
+REM x86 test run:
+bin\test_env.exe
 
 pause
