88a8e7bb45a54cda9865977abb35c519069ac966
Miloslav Beňo <miloslav@devsense.com>
2015-02-07 17:28:51 +0000

    - GlobalScope support for creating instances - Echo method in GlobalScope added
    Originally checked in by DEVSENSE\Miloslav on 2011-12-09 02:51:36.11 as Changeset 2216
    
    git-tfs-id: [https://devsense.visualstudio.com/DefaultCollection]$/Phalanger/Main;C6355

--- Source/Core/Utilities/GlobalScope.cs
+++ Source/Core/Utilities/GlobalScope.cs
@@ -17,6 +17,47 @@ namespace PHP.Core.Utilities
     public sealed class GlobalScope : DynamicObject
     {
         private ScriptContext context;
+        private ClassesScope classes;
+
+        private ClassesScope Classes
+        {
+            get
+            {
+                if (classes == null)
+                    classes = new ClassesScope(this);
+
+                return classes;
+            }
+
+        }
+
+        /// <summary>
+        /// Class for calling constructors of php classes
+        /// </summary>
+        internal class ClassesScope : DynamicObject
+        {
+            private GlobalScope globals;
+
+            internal ClassesScope(GlobalScope globals)
+            {
+                this.globals = globals;
+            }
+
+            /// <summary>
+            /// Specifies dynamic behavior for invoke operation for global function
+            /// </summary>
+            public override bool TryInvokeMember(
+                InvokeMemberBinder binder,
+                Object[] args,
+                out Object result
+            )
+            {
+                result = globals.context.NewObject(binder.Name, wrapArgs(args));
+
+                return true;
+            }
+        }
+
 
         /// <summary>
         /// Initialize GlobalScope object
@@ -38,16 +79,20 @@ namespace PHP.Core.Utilities
             out Object result
         )
         {
+            result = PhpVariable.Dereference(context.Call(binder.Name, null, null, wrapArgs(args)));
+            return true;
+        }
+
+        private static object[] wrapArgs(Object[] args)
+        {
             object[] wrappedArgs = new object[args.Length];
 
-            for(int i = 0; i < args.Length; ++i)
+            for (int i = 0; i < args.Length; ++i)
             {
                 Debug.Assert(!(args[i] is PhpReference));
                 wrappedArgs[i] = ClrObject.WrapDynamic(args[i]);
             }
-
-            result = PhpVariable.Dereference(context.Call(binder.Name, null, null, wrappedArgs));
-            return true;
+            return wrappedArgs;
         }
 
         /// <summary>
@@ -58,6 +103,13 @@ namespace PHP.Core.Utilities
             out Object result
         )
         {
+            if (binder.Name == "new")
+            {
+                result = Classes;
+                return true;
+            }
+
+
             context.GlobalVariables.TryGetValue(binder.Name, out result);
             result = PhpVariable.Dereference(result);
 
@@ -73,9 +125,76 @@ namespace PHP.Core.Utilities
         )
         {
             Debug.Assert(!(value is PhpReference));
-            
+
             context.GlobalVariables[binder.Name] = ClrObject.WrapDynamic(value);
             return true;
+        }
+
+        #endregion
+
+        #region Echo
+
+        /// <summary>
+        /// Writes data to the current output.
+        /// </summary>
+        /// <param name="value">Data to be written.</param>
+        public void Echo(object value)
+        {
+            ScriptContext.Echo(value, context);
+        }
+
+        /// <summary>
+        /// Writes <see cref="PhpBytes" /> data to the current output.
+        /// </summary>
+        /// <param name="value">Data to be written.</param>
+        public void Echo(PhpBytes value)
+        {
+            ScriptContext.Echo(value, context);
+        }
+
+        /// <summary>
+        /// Writes <see cref="string" /> to the current output.
+        /// </summary>
+        /// <param name="value">The string to be written.</param>
+        public void Echo(string value)
+        {
+            ScriptContext.Echo(value, context);
+        }
+
+        /// <summary>
+        /// Writes <see cref="bool" /> to the current output.
+        /// </summary>
+        /// <param name="value">The boolean to be written.</param>
+        public void Echo(bool value)
+        {
+            ScriptContext.Echo(value, context);
+        }
+
+        /// <summary>
+        /// Writes <see cref="int" /> to the current output.
+        /// </summary>
+        /// <param name="value">The integer to be written.</param>
+        public void Echo(int value)
+        {
+            ScriptContext.Echo(value, context);
+        }
+
+        /// <summary>
+        /// Writes <see cref="long"/> to the current output.
+        /// </summary>
+        /// <param name="value">The long integer to be written.</param>
+        public void Echo(long value)
+        {
+            ScriptContext.Echo(value, context);
+        }
+
+        /// <summary>
+        /// Writes <see cref="double"/> to the current output.
+        /// </summary>
+        /// <param name="value">The double to be written.</param>
+        public void Echo(double value)
+        {
+            ScriptContext.Echo(value, context);
         }
 
         #endregion
