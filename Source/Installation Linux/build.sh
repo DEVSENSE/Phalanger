0eadc4f288ce706235f63557ea96d11006f474fb
Maroš Beňo <maros.beno@azure.devsense.com>
2015-02-07 17:29:19 +0000

    Removed MySql from the basic Linux Installation
    Originally checked in by DEVSENSE\maros.beno on 2011-12-13 23:13:23.793 as Changeset 2225
    
    git-tfs-id: [https://devsense.visualstudio.com/DefaultCollection]$/Phalanger/Main;C6364

--- Source/Installation Linux/build.sh
+++ Source/Installation Linux/build.sh
@@ -6,4 +6,5 @@ cp "..\..\Deployment\Bin\PhpNetCore.IL.dll" "Debian package\usr\lib\phalanger\bi
 cp "..\..\Deployment\Bin\PhpNetCore.dll" "Debian package\usr\lib\phalanger\bin"
 cp "..\..\Deployment\Bin\PhpNetClassLibrary.dll" "Debian package\usr\lib\phalanger\bin"
 cp "..\..\Deployment\Bin\PhpNetXmlDom.dll" "Debian package\usr\lib\phalanger\bin"
-dpkg -b "Debian package" "Deployment\Bin\Package\phalanger_$phalanger_version-$architecture"
+dpkg -b "Debian package" "..\..\Deployment\Bin\Package\phalanger_$phalanger_version-$architecture"
+alien "..\..\Deployment\Bin\Package\phalanger_$phalanger_version-$architecture"
--- Source/Installation Linux/prepare.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo -s
-apt-get install build-essential dpkg
