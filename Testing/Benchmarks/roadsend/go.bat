99bd4d3da9c0a8f9e251c0ba7379b2c86088e7b9
Jakub Míšek <jmisek@gmail.com>
2015-02-07 17:35:57 +0000

    Benchmarks maintenance (outputs/merges CSV, runs in x64/x86, runs PHP 5.3 and PHP 5.4; have to be configured in go_php.bat)
    Originally checked in by DEVSENSE\Jakub on 2012-02-19 21:40:50.83 as Changeset 2431
    
    git-tfs-id: [https://devsense.visualstudio.com/DefaultCollection]$/Phalanger/Main;C6484

--- Testing/Benchmarks/roadsend/go.bat
+++ Testing/Benchmarks/roadsend/go.bat
@@ -1,11 +1,8 @@
 ..\..\..\Deployment\Bin\phpc /debug- /config:App.config test_env.php
 ..\..\..\Tools\peverify bin\test_env.exe
-
-REM x64 test run:
-bin\test_env.exe
+bin\test_env.exe results.csv
 
 "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\corflags.exe" /32BIT+ bin\test_env.exe
-REM x86 test run:
-bin\test_env.exe
+bin\test_env.exe results.csv
 
 pause
