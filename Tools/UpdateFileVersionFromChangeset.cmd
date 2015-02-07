787910fbda9e615e0fe3894343411f56da4d1d57
Jakub Míšek <jmisek@gmail.com>
2015-02-07 18:29:41 +0000

    Cleanup
    Originally checked in by DEVSENSE\Jakub on 2014-03-31 17:42:57.2 as Changeset 5103
    
    git-tfs-id: [https://devsense.visualstudio.com/DefaultCollection]$/Phalanger/Main;C7181

--- Tools/UpdateFileVersionFromChangeset.cmd
+++ Tools/UpdateFileVersionFromChangeset.cmd
@@ -7,6 +7,7 @@ tf history . /r /noprompt /stopafter:1 /version:W > tf.output 2>nul
 SET CHANGESET=0
 FOR /F "tokens=1 skip=2" %%N IN (tf.output) DO @SET CHANGESET=%%N
 del /F /Q tf.output
+if [%CHANGESET%]==[0] (goto :main)
 
 :start
 if "%1"=="" (goto :main)
