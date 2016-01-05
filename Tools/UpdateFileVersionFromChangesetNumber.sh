e47466d7585d0e3eccd80394b02bbbae440aa721
Bruce Weirdan <weirdan@gmail.com>
2016-01-06 01:19:13 +0200

    build fixes for mono (xbuild)
    Added:
    	* unix variant of UpdateVileVersionFromChangeset
    
    Changed:
    	* made VersionReplacer.exe executable
    	* disabled windows-specific snippets in Common.Build.targets on unix
    	  plaform
    	* added mono-specific switch to prevent csc task from adding
    	  /nostdlib switch to csc invocation

---
+++ Tools/UpdateFileVersionFromChangesetNumber.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# determining changeset number (just the last git-tfs-id)
+CHANGESET=$(git log --grep "git-tfs-id" --max-count=1 | grep git-tfs-id | egrep -o '[[:digit:]]+$')
+while [ -n "$1" ] ; do
+	"$(dirname $0)/VersionReplacer.exe" "$1" "$CHANGESET"
+	shift
+done
