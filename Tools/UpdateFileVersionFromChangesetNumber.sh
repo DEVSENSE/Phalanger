2676a704f16009caad78de1a0e17ced071955431
Bruce Weirdan <weirdan@gmail.com>
2016-01-25 03:09:00 +0200

    quoting for changeset number

--- Tools/UpdateFileVersionFromChangesetNumber.sh
+++ Tools/UpdateFileVersionFromChangesetNumber.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # determining changeset number (just the last git-tfs-id)
-CHANGESET=$(git log --grep "git-tfs-id" --max-count=1 | grep git-tfs-id | egrep -o '[[:digit:]]+$')
+CHANGESET="$(git log --grep "git-tfs-id" --max-count=1 | grep git-tfs-id | egrep -o '[[:digit:]]+$')"
 while [ -n "$1" ] ; do
 	"$(dirname $0)/VersionReplacer.exe" "$1" "$CHANGESET"
 	shift
